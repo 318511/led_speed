3be675bfaf82a54f4d5460b0d57bd815b1342d76
318511 <91246969+318511@users.noreply.github.com>
2021-12-23 15:40:22 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,45 +1,72 @@
-input.onButtonPressed(Button.A, function () {
-    方向 = 1
-})
-input.onButtonPressed(Button.AB, function () {
-    方向 = 3
-})
-function 燈 (YY: number, 速度: number, 方向: number) {
-    if (方向 == 0) {
-        for (let index = 0; index < 10; index++) {
+/**
+ * 我有註解!!!
+ * 
+ * 需要點開
+ */
+// 這裡共有4串程式，都長得差不多
+function 燈 (YY: number, 速度: number) {
+    // 一定要!這樣才會重複判斷
+    while (true) {
+        while (方向 % 4 == 0) {
+            // 讓燈跑
             for (let X = 0; X <= 4; X++) {
-                if (list[X][YY] == 1) {
-                    led.plot(YY, X)
-                } else {
-                    led.unplot(YY, X)
-                }
-            }
-            list[YY].unshift(list[YY].pop())
-            basic.pause(速度)
-        }
-    } else if (方向 == 1) {
-        for (let index = 0; index < 10; index++) {
-            for (let X = 0; X <= 4; X++) {
-                if (list[X][YY] == 1) {
+                // 把陣列轉到顯示
+                if (list[YY][X] == 1) {
                     led.plot(X, YY)
                 } else {
                     led.unplot(X, YY)
                 }
             }
+            // 往右
+            list[YY].unshift(list[YY].pop())
+            // 控制速度
+            basic.pause(速度)
+        }
+        while (方向 % 4 == 1) {
+            for (let X = 0; X <= 4; X++) {
+                if (list[YY][X] == 1) {
+                    led.plot(YY, X)
+                } else {
+                    led.unplot(YY, X)
+                }
+            }
+            // 往下
             list[YY].unshift(list[YY].pop())
             basic.pause(速度)
         }
-    } else if (方向 == 2) {
-    	
-    } else if (方向 == 3) {
-    	
+        while (方向 % 4 == 2) {
+            for (let X = 0; X <= 4; X++) {
+                if (list[YY][X] == 1) {
+                    led.plot(X, YY)
+                } else {
+                    led.unplot(X, YY)
+                }
+            }
+            // 往左
+            list[YY].push(list[YY].shift())
+            basic.pause(速度)
+        }
+        while (方向 % 4 == 3) {
+            for (let X = 0; X <= 4; X++) {
+                if (list[YY][X] == 1) {
+                    led.plot(YY, X)
+                } else {
+                    led.unplot(YY, X)
+                }
+            }
+            // 往上
+            list[YY].push(list[YY].shift())
+            basic.pause(速度)
+        }
     }
 }
+// 用變數「方向」來控制燈跑的......方向
 input.onButtonPressed(Button.B, function () {
-    方向 = 2
+    方向 += 1
 })
 let 方向 = 0
 let list: number[][] = []
+// 阿....就陣列
 list = [
 [
 1,
@@ -77,19 +104,25 @@ list = [
 0
 ]
 ]
+// 歸0
 方向 = 0
+// 每一個速度移動的燈的數據
 control.inBackground(function () {
-    燈(0, 500, 方向)
+    燈(0, 500)
 })
+// 每一個速度移動的燈的數據
 control.inBackground(function () {
-    燈(1, 400, 方向)
+    燈(1, 400)
 })
+// 每一個速度移動的燈的數據
 control.inBackground(function () {
-    燈(2, 300, 方向)
+    燈(2, 300)
 })
+// 每一個速度移動的燈的數據
 control.inBackground(function () {
-    燈(3, 200, 方向)
+    燈(3, 200)
 })
+// 每一個速度移動的燈的數據
 control.inBackground(function () {
-    燈(4, 100, 方向)
+    燈(4, 100)
 })
